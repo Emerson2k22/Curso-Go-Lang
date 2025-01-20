0d5a93c7568333d09ce0dd73fd37f741e050e8e0
Emerson João <emersonjoao0800@gmail.com>
2025-01-20 12:17:34 -0300

    PrimeiroCommit

---
+++ main.go
@@ -0,0 +1,23 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("Hello, world!")
+	fmt.Println("Começando meus primeiros passos com a linguagem GoLang!")
+
+//Tipos 
+
+//bool (true/false)
+	fmt.Printf("Type: %T - Value: %v\n", true, true)
+
+// string - sequência de bytes
+    fmt.Printf("Type: %T - Value: %v\n", "steph", "steph")
+	fmt.Printf("Type: %T - Value: %v\n", "1", "1")
+
+// inteiro
+	fmt.Printf("Type: %T - Value: %v\n", 2, 2)
+
+// float (float64/float32) - decimal
+    fmt.Printf("Type: %T - Value: %v\n", 1.233, 1.233)
+}
