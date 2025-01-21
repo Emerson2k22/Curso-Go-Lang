16b741e02d21355e5c38827a27adcd98033c733b
Emerson João <emersonjoao0800@gmail.com>
2025-01-21 08:59:43 -0300

    Update

--- main.go
+++ main.go
@@ -21,4 +21,38 @@ func main() {
 
 // float (float64/float32) - decimal
     fmt.Printf("Type: %T - Value: %v\n", 1.233, 1.233)
+
+	// Variáveis
+	//var + nome da variável + tipo
+	var nome string
+	nome = "bento"
+	fmt.Println(nome)
+
+	// var int
+	var idade int
+	idade = 4
+	fmt.Println(idade)
+	// ou para mais de uma variável
+	var b, c int = 1, 2
+	fmt.Println(b)
+	fmt.Println(c)
+
+	// variável bool
+	var d = true
+	fmt.Println(d)
+
+    // variável abreviada
+	f := "apple"
+	fmt.Println(f)
+
+	// constantes
+	const idadeBento = 24
+	fmt.Println(idadeBento)
+
+	// funções
+	fmt.Println(soma(42, 13))
+}
+
+func soma (x int, y int) int {
+	return x + y
 }
